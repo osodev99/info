eeb566913b5eccf52167a3f59ef44b122c5cb8c3
adalid <alimachi@icei.tech>
2024-11-05 16:23:53 -0400

    add carrousel in somos

--- somos.php
+++ somos.php
@@ -1,86 +1,82 @@
 
 <?php include("template/cabecera3.php");?>
-                <main>
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-                        
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                            <img src="https://fondosmil.com/fondo/70409.jpg" class="d-block w-100 " height=500vh alt="slider 1" >
-                                <div class="carousel-caption top-0 mt-4">
-                                    <p class="mt-5 fs-3 test-uppercase">
-                                    "La informacion es Util cuando es Comprendida"
-                                    </p>
-                                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
-                                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
-                                </div>
-                            </div>
-                            <div class="carousel-item d-item">
-                            <img src="https://p4.wallpaperbetter.com/wallpaper/735/780/874/blur-class-seminar-wallpaper-preview.jpg" class="d-block w-100 d-img" height=500vh alt="slider 1">
-                                <div class="carousel-caption top-0 mt-4">
-                                    <p class="mt-5 fs-3 test-uppercase">
-                                    "Cree en ti mismo"
-                                    </p>
-                                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
-                                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
-                                </div>
-                            </div>
-                            <div class="carousel-item d-item">
-                            <img src="https://p4.wallpaperbetter.com/wallpaper/752/335/746/class-estudiantes-profesor-wallpaper-preview.jpg" class="d-block w-100 d-img" height=500vh alt="slider 1">
-                                <div class="carousel-caption top-0 mt-4">
-                                    <p class="mt-5 fs-3 test-uppercase">
-                                    "La informacion es Util cuando es Comprendida"
-                                    </p>
-                                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
-                                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                                   
-                    <div class="container-fluid px-4">
-                     
-                    <h1 class="mt-4">MISIÓN</h1>
-                            <ol class="breadcrumb mb-4">
-                                <li class="breadcrumb-item active">
-                                    <p style="text-align: justify;">La misión de nuestro sistema de seguimiento académico para la educación superior es proporcionar una plataforma integral y eficiente que facilite la gestión y supervisión del rendimiento académico de los estudiantes.
-                                     Nos comprometemos a ofrecer herramientas innovadoras que apoyen el proceso de gestión de datos.
-                                    <p>
-                                    </p>
-                                    Buscamos contribuir al éxito educativo, proporcionando información en tiempo real y recursos que potencien el crecimiento académico de los estudiantes y fortalezcan la calidad de la educación superior.
+<main>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/carousel-1.jpg" class="img-fluid d-block w-100 " height=500vh alt="slider 1" >
+                <div class="carousel-caption top-0 mt-4">
+                    <p class="mt-5 fs-3 test-uppercase">
+                    "La informacion es Util cuando es Comprendida"
+                    </p>
+                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
+                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
+                </div>
+            </div>
+            <div class="carousel-item d-item">
+            <img src="img/carousel-2.jpg" class="img-fluid d-block w-100 d-img" height=500vh alt="slider 1">
+                <div class="carousel-caption top-0 mt-4">
+                    <p class="mt-5 fs-3 test-uppercase">
+                    "Cree en ti mismo"
+                    </p>
+                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
+                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
+                </div>
+            </div>
+            <div class="carousel-item d-item">
+            <img src="img/carousel-3.jpg" class="img-fluid d-block w-100 d-img" height=500vh alt="slider 1">
+                <div class="carousel-caption top-0 mt-4">
+                    <p class="mt-5 fs-3 test-uppercase">
+                    "La informacion es Util cuando es Comprendida"
+                    </p>
+                    <h1 class="display-1 fw-bolder test-capitalize">Innovatech Solutions</h1>
+                    <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Informacion</button>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+                   
+    <div class="container-fluid px-4">
+     
+    <h1 class="mt-4">MISIÓN</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">
+                    <p style="text-align: justify;">La misión de nuestro sistema de seguimiento académico para la educación superior es proporcionar una plataforma integral y eficiente que facilite la gestión y supervisión del rendimiento académico de los estudiantes.
+                     Nos comprometemos a ofrecer herramientas innovadoras que apoyen el proceso de gestión de datos.
+                    <p>
+                    </p>
+                    Buscamos contribuir al éxito educativo, proporcionando información en tiempo real y recursos que potencien el crecimiento académico de los estudiantes y fortalezcan la calidad de la educación superior.
 
-                                    </p>    
-                                </li>
-                            </ol>
-                    <h1 class="mt-4">VISIÓN</h1>
-                            <ol class="breadcrumb mb-4">
-                                <li class="breadcrumb-item active">
-                                    <p style="text-align: justify;">
-                                    Nuestra visión es ser reconocidos como el sistema de seguimiento académico líder en la educación superior, destacándonos por nuestra excelencia, innovación y compromiso con la mejora continua.
-                                    <p>
-                                    </p>
-                                    Aspiramos a ser la plataforma preferida por estudiantes, docentes y administradores, brindando soluciones intuitivas, personalizadas y centradas en el rendimiento académico. Buscamos ser un socio estratégico para las instituciones educativas, 
-                                    contribuyendo al éxito de los estudiantes mediante la optimización de procesos, la retroalimentación efectiva y la promoción de un entorno educativo que fomente el aprendizaje significativo en el ámbito de la educación superior.
-
-                                    <p>
-                                    </p>
-                                 
-                                    </p>
-                                    </li>
-                            </ol>
-                                           
-                    </div>
-                </main>
+                    </p>    
+                </li>
+            </ol>
+    <h1 class="mt-4">VISIÓN</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">
+                    <p style="text-align: justify;">
+                        Nuestra visión es ser reconocidos como el sistema de seguimiento académico líder en la educación superior, destacándonos por nuestra excelencia, innovación y compromiso con la mejora continua.
+                    <p>
+                    <p>
+                        Aspiramos a ser la plataforma preferida por estudiantes, docentes y administradores, brindando soluciones intuitivas, personalizadas y centradas en el rendimiento académico. Buscamos ser un socio estratégico para las instituciones educativas, 
+                        contribuyendo al éxito de los estudiantes mediante la optimización de procesos, la retroalimentación efectiva y la promoción de un entorno educativo que fomente el aprendizaje significativo en el ámbito de la educación superior.
+                    </p>
+                </li>
+            </ol>
+                           
+    </div>
+</main>
 <?php include("template/pie.php");?>
